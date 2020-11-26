251d285ad5b694345b9d32d4563a55841e59babe
Edik Daukaev <daukaev.eduard@mail.ru>
2020-11-27 04:26:28 +0500

    Второе изменение

--- Лаба4.ООП/Form1.Designer.cs
+++ Лаба4.ООП/Form1.Designer.cs
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.picture = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picture
+            // 
+            this.picture.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.picture.Location = new System.Drawing.Point(0, 0);
+            this.picture.Name = "picture";
+            this.picture.Size = new System.Drawing.Size(800, 450);
+            this.picture.TabIndex = 0;
+            this.picture.TabStop = false;
+            this.picture.MouseClick += new System.Windows.Forms.MouseEventHandler(this.picture_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picture);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picture)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox picture;
     }
 }
 
--- Лаба4.ООП/Form1.cs
+++ Лаба4.ООП/Form1.cs
@@ -16,5 +16,23 @@ namespace Лаба4.ООП
         {
             InitializeComponent();
         }
+        public class CCircle
+        {
+            public int R;
+            public int x, y;
+            public CCircle(int x,int y)
+            {
+                this.x = x;
+                this.y = y;
+            }
+            ~CCircle() { 
+            }
+        }
+
+        private void picture_MouseClick(object sender, MouseEventArgs e)
+        {
+            CCircle krug = new CCircle(e.X, e.Y);
+
+        }
     }
 }
