32efc47e3a1cc42e8056b4240f0fa15843ded17f
Edik Daukaev <daukaev.eduard@mail.ru>
2020-11-27 15:59:38 +0500

    Второе изменение

--- Лаба4.ООП/Form1.cs
+++ Лаба4.ООП/Form1.cs
@@ -28,11 +28,46 @@ namespace Лаба4.ООП
             ~CCircle() { 
             }
         }
+        private void MyPaint(int size,ref Storage storage)
+        {
+            if(storage.objects[size-1]!= null)
+            {
+                Pen pen = new Pen(storage.objects[size - 1].color, 4);
+                picture.CreateGraphics().DrawEllipse(pen,)
+            }
+        } 
 
         private void picture_MouseClick(object sender, MouseEventArgs e)
         {
             CCircle krug = new CCircle(e.X, e.Y);
 
+        }
+        class Storage
+        {
+            public CCircle[] objects;
+            public Storage(int size)
+            {
+                objects = new CCircle[size];
+                for (int i = 0; i < size; i++)
+                    objects[i] = null;
+            }
+            public void add_object(int size, CCircle new_object)
+            {
+                Storage storage1 = new Storage(size + 1);
+                for (int i = 0; i < size; i++)
+                    storage1.objects[i] = objects[i];
+                size = size + 1;
+                videlenie(size);
+                for (int i = 0; i < size; i++)
+                    objects[i] = storage1.objects[i];
+                objects[size - 2] = new_object;
+            }
+            public void videlenie(int size)
+            {
+                objects = new CCircle[size];
+                for (int i = 0; i < size; i++)
+                    objects[i] = null;
+            }
         }
     }
 }
