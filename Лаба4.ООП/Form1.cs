e0bb3a7bb39502dcfc987e8ffdefd4ece0dfc88c
Edik Daukaev <daukaev.eduard@mail.ru>
2020-11-28 07:23:00 +0500

    Изменение 4

--- Лаба4.ООП/Form1.cs
+++ Лаба4.ООП/Form1.cs
@@ -12,38 +12,72 @@ namespace Лаба4.ООП
 {
     public partial class Form1 : Form
     {
+        Bitmap bmp;
+        Graphics graph;
+        Color basic_color = Color.DarkGray;
+        int kolvo_elem = 0;
+        int index = 0;
+        static int sizeStorage = 1;
+        int index_sozdania;
+        Storage storage = new Storage(sizeStorage);
         public Form1()
         {
             InitializeComponent();
+            bmp = new Bitmap(picture.Width, picture.Height);
+            graph = Graphics.FromImage(bmp);
         }
         public class CCircle
         {
-            public int R;
+            public bool painted = true;
+            public int R=80;
+            public Color color;
             public int x, y;
-            public CCircle(int x,int y)
+            public CCircle(int x,int y,Color color)
             {
-                this.x = x;
-                this.y = y;
+                this.x = x-R;
+                this.y = y-R;
+                this.color = color;
             }
             ~CCircle() { 
             }
+        }
+        private int Check_In(ref Storage storage,int size,int x,int y)
+        {
+            if (storage.kolvo_zanyatix(size)!=0) 
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    if (!storage.proverka(i))
+                    {
+                        if ((x - storage.objects[i].x) * (x - storage.objects[i].x) + (y - storage.objects[i].y) * (y - storage.objects[i].y) <= storage.objects[i].R * storage.objects[i].R)
+                            return i;
+                    }
+                }
+
+            }
+            return -1;
         }
         private void MyPaint(int size,ref Storage storage)
         {
             if(storage.objects[size-1]!= null)
             {
                 Pen pen = new Pen(storage.objects[size - 1].color, 4);
-                picture.CreateGraphics().DrawEllipse(pen,)
+                graph.DrawEllipse(pen, storage.objects[size-1 ].x, storage.objects[size-1 ].y, storage.objects[size-1 ].R * 2, storage.objects[size-1 ].R * 2);
+                picture.Image = bmp;
             }
         } 
 
         private void picture_MouseClick(object sender, MouseEventArgs e)
         {
-            CCircle krug = new CCircle(e.X, e.Y);
+            CCircle krug = new CCircle(e.X, e.Y,basic_color);
+            storage.add_object(ref sizeStorage, ref krug, kolvo_elem, ref index_sozdania);
+            MyPaint(sizeStorage, ref storage) ;
+            picture.Image = bmp;
 
         }
-        class Storage
+        public class Storage
         {
+           
             public CCircle[] objects;
             public Storage(int size)
             {
@@ -51,16 +85,17 @@ namespace Лаба4.ООП
                 for (int i = 0; i < size; i++)
                     objects[i] = null;
             }
-            public void add_object(int size, CCircle new_object)
+            public void add_object(ref int size, ref CCircle new_object,int ind,ref int index_sozdania)
             {
                 Storage storage1 = new Storage(size + 1);
                 for (int i = 0; i < size; i++)
                     storage1.objects[i] = objects[i];
                 size = size + 1;
-                videlenie(size);
+                    videlenie(size);
                 for (int i = 0; i < size; i++)
                     objects[i] = storage1.objects[i];
-                objects[size - 2] = new_object;
+                objects[ind] = new_object;
+                index_sozdania = ind;
             }
             public void videlenie(int size)
             {
@@ -68,6 +103,24 @@ namespace Лаба4.ООП
                 for (int i = 0; i < size; i++)
                     objects[i] = null;
             }
+           public int kolvo_zanyatix(int size)
+            {
+                int count_zanyatih = 0;
+                for(int i=0;i < size; i++)
+                {
+                    if (!proverka(i))
+                        count_zanyatih++;
+                }
+                return count_zanyatih;
+            }
+            public bool proverka(int kolvo_elem)
+            {
+                if (objects[kolvo_elem] == null)
+                    return true;
+                else return false;
+            }
+
+            
         }
     }
 }
